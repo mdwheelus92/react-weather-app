124c70e89a40dd1d22ca4cf1088e49999fd63254
mdwheelus92 <michellewheelus@gmail.com>
2023-01-03 14:59:33 -0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/mdwheelus92/react-weather-app"
-          target="_blank"
-        >
-          Open-Sourced Code{" "}
-        </a>
-        by: Michelle Wheelus
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/mdwheelus92/react-weather-app"
+            target="_blank"
+          >
+            Open-Sourced Code{" "}
+          </a>
+          by: Michelle Wheelus
+        </footer>
+      </div>
     </div>
   );
 }
