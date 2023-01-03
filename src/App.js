6ef43e4319ac1672368fc798770b439da057f211
mdwheelus92 <michellewheelus@gmail.com>
2023-01-03 14:35:05 -0800

    Added Git repository link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/mdwheelus92/react-weather-app"
+          target="_blank"
+        >
+          Open-Sourced Code{" "}
+        </a>
+        by: Michelle Wheelus
+      </footer>
+    </div>
+  );
 }
 
 export default App;
